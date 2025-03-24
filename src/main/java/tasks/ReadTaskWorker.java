8055fd5209615d83463de760b50f51ee7badc616
stevanovic.uros <stevanovic.uros@credo-company.com>
2025-03-24 13:28:58 +0100

    Change from busy loop to event thread

--- src/main/java/tasks/ReadTaskWorker.java
+++ src/main/java/tasks/ReadTaskWorker.java
@@ -4,75 +4,96 @@ import core.PeerConnection;
 import piece.*;
 
 import java.nio.ByteBuffer;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
-public class ReadTaskWorker implements Runnable {
+public class ReadTaskWorker {
 
-    private final Task task;
+    private final BlockingQueue<Task> tasks;
     private final RarestFirstPicker rarestFirstPicker;
+    private final ExecutorService executorService;
 
-    public ReadTaskWorker(Task task, RarestFirstPicker rarestFirstPicker) {
-        this.task = task;
+    private Thread consumerThread;
+    private volatile boolean running = false;
+
+    public ReadTaskWorker(RarestFirstPicker rarestFirstPicker) {
+        this.tasks = new LinkedBlockingQueue<>();
         this.rarestFirstPicker = rarestFirstPicker;
+        this.executorService = Executors.newFixedThreadPool(10);
     }
 
 
-    @Override
-    public void run() {
-        Message message = determineMessage(task);
-        if(message != null){
-            message.parse(ByteBuffer.wrap(task.getMessage()));
+    public synchronized void start() {
+        if (running) {
+            throw new IllegalStateException("ReadTaskWorker is already running");
+        }
+        running = true;
+        consumerThread = new Thread(this::consumeTasks, "ReadTaskWorker");
+        consumerThread.start();
+    }
+
+    private void consumeTasks() {
+        while (running) {
+            try {
+                Task task = tasks.take();
+                executorService.execute(() -> {
+                    Message message = determineMessage(task);
+                    if (message != null) {
+                        message.parse(ByteBuffer.wrap(task.getMessage()));
+                    }
+                });
+
+            } catch (InterruptedException e) {
+                if (!running) {
+                    break;
+                }
+                Thread.currentThread()
+                        .interrupt();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
+
+    public synchronized void stop() {
+        running = false;
+
+        if (consumerThread != null) {
+            consumerThread.interrupt();
+        }
+        executorService.shutdown();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
     private Message determineMessage(Task task) {
+
+        if (task == null) {
+            return null;
+        }
+
         byte[] message = task.getMessage();
         ByteBuffer buffer = ByteBuffer.wrap(message);
-        int length = buffer.getInt();
+        buffer.getInt();
         byte id = buffer.get();
         PeerConnection peerConnection = task.getPeerConnection();
-        System.out.println("Determining message for task with id: " + id + " and length: " + length);
 
         return switch (id) {
-            case 0 -> {
-                System.out.println("Task is Choke");
-                yield new Choke(peerConnection);
-            }
-            case 1 -> {
-                System.out.println("Task is Unchoke");
-                yield new Unchoke(peerConnection);
-            }
-            case 2 -> {
-                System.out.println("Task is Interested");
-                yield new Interested(peerConnection);
-            }
-            case 3 -> {
-                System.out.println("Task is NotInterested");
-                yield new NotInterested(peerConnection);
-            }
-            case 4 -> {
-                System.out.println("Task is Have");
-                yield new Have(0, peerConnection);
-            }
-            case 5 -> {
-                System.out.println("Task is BitField");
-                yield new BitField(new byte[0], peerConnection);
-            }
-            case 6 -> {
-                System.out.println("Task is Request");
-                yield new Request(rarestFirstPicker, peerConnection);
-            }
-            case 7 -> {
-                System.out.println("Task is Piece");
-                yield new Piece(0, 0, new byte[0], peerConnection);
-            }
-            case 8 -> {
-                System.out.println("Task is Cancel");
-                yield new Cancel(peerConnection);
-            }
-            default -> {
-                System.out.println("Task with unknown id: " + id);
-                yield null;
-            }
+            case 0 -> new Choke(peerConnection);
+            case 1 -> new Unchoke(peerConnection);
+            case 2 -> new Interested(peerConnection);
+            case 3 -> new NotInterested(peerConnection);
+            case 4 -> new Have(0, peerConnection);
+            case 5 -> new BitField(new byte[0], peerConnection);
+            case 6 -> new Request(rarestFirstPicker, peerConnection);
+            case 7 -> new Piece(0, 0, new byte[0], peerConnection);
+            case 8 -> new Cancel(peerConnection);
+            default -> null;
         };
     }
 
