0d088570fd3acb114df5e705c5f7223dd6604110
stevanovic.uros <stevanovic.uros@credo-company.com>
2025-03-24 13:28:29 +0100

    Use array instead of list

--- src/main/java/storage/PieceStorage.java
+++ src/main/java/storage/PieceStorage.java
@@ -4,18 +4,20 @@ import piece.Block;
 
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PieceStorage {
 
     private final int index;
-    private final List<Block> blocks;
+    private final Block[] blocks;
     private final ByteBuffer dataBuffer;
     private final byte[] expectedHash;
     private boolean verified;
+    private final AtomicInteger blocksCompleted = new AtomicInteger(0);
+    private final AtomicBoolean requested = new AtomicBoolean(false);
+    private final AtomicBoolean finished = new AtomicBoolean(false);
 
 
     public PieceStorage(int index, int pieceLength, byte[] expectedHash) {
@@ -23,40 +25,72 @@ public class PieceStorage {
         this.expectedHash = expectedHash;
         this.dataBuffer = ByteBuffer.allocate(pieceLength);
         this.verified = false;
+
+        int numberOfBlocks = (int) Math.ceil((double) pieceLength / 16384);
+        this.blocks = new Block[numberOfBlocks];
         int offset = 0;
-        this.blocks = new ArrayList<>();
-        while (offset < pieceLength) {
+
+        for (int i = 0; i < numberOfBlocks; i++) {
             int blockLength = Math.min(16384, pieceLength - offset);
-            blocks.add(new Block(offset, blockLength, 0, index));
+            blocks[i] = new Block(offset, blockLength, 0, index);
             offset += blockLength;
         }
     }
 
-
-    public synchronized boolean updateBlock(int offset, byte[] blockData) {
-        for (Block block : blocks) {
-            if (block.getOffset() == offset && block.getDownloadState() == 2) {
-                dataBuffer.position(offset);
-                dataBuffer.put(blockData);
-                block.setDownloadState(1);
-                System.out.println("UPDATED BLOCK WITH OFFSET " + offset + " and state " + block.getDownloadState());
-                return true;
-            }
-        }
-        return false;
+    public int getAmountOfDownloadedBytes(){
+        return blocksCompleted.get() * 16384;
     }
 
-    public boolean areAllBlocksDownloaded() {
+    public synchronized boolean updateBlock(int offset, byte[] blockData) {
+
+        int blockIndex = offset / 16384;
+        Block block = blocks[blockIndex];
+
+        if(block.getOffset() != offset){
+            return false;
+        }
+
+        if(block.getDownloadState() == 1){
+            return false;
+        }
+
+        block.setDownloadState(1);
+        dataBuffer.position(offset);
+        dataBuffer.put(blockData);
+        int i = blocksCompleted.incrementAndGet();
+
+        if(i == blocks.length){
+            System.out.println("ALL BLOCKS DOWNLOADED");
+            verify();
+        }
+
+        blocks[blockIndex] = block;
+
+       return true;
+
+    }
+
+    public boolean isFinished(){
+        return finished.get();
+    }
+
+    public boolean isRequested(){
+        return requested.get();
+    }
+
+    public boolean areAllBlockRequested(){
+        if(requested.get()) return true;
+
         for (Block block : blocks) {
-            if (block.getDownloadState() != 1) {
-                if(block.getRequestTime().plusSeconds(3).isBefore(LocalDateTime.now())){
-                    block.setDownloadState(0);
-                }
+            if (block.getDownloadState() == 0) {
                 return false;
             }
         }
+        requested.set(true);
         return true;
     }
+
+
 
     public synchronized Block getNextBlock() {
 
@@ -68,13 +102,6 @@ public class PieceStorage {
             }
         }
 
-        if (areAllBlocksDownloaded()) {
-            System.out.println("Should come here?");
-            if(verify()){
-              //downloadPiece
-
-            };
-        }
         return null;
     }
 
@@ -84,6 +111,7 @@ public class PieceStorage {
             byte[] hash = digest.digest(dataBuffer.array());
             if (MessageDigest.isEqual(hash, expectedHash)) {
                 verified = true;
+                finished.set(true);
                 System.out.println("HASHES EQUAL");
                 return true;
             } else {
@@ -92,7 +120,25 @@ public class PieceStorage {
             }
         }catch (Exception e){
             e.printStackTrace();
+            return false;
         }
+
+    }
+
+
+    public synchronized void clearStale(){
+        if (!finished.get() && isRequested()) {
+            for (Block block : blocks) {
+                if (block.getDownloadState() == 2) {
+                    block.setDownloadState(0);
+                    requested.set(false);
+                }
+            }
+        }
+    }
+
+    public byte[] getData(){
+        return dataBuffer.array();
     }
 
     public boolean isVerified() {
